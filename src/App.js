9baf1c57b7b6cbd7639437c951259f1e1936d723
IG <iikim2511@gmail.com>
2023-08-14 23:24:22 -0400

    navbar.js

--- src/App.js
+++ src/App.js
@@ -9,18 +9,13 @@ import Cart from './component/Cart';
 const App = () => {
   return (
     <div>
-    
-        <h2>App</h2>
-
-        <Navbar/>
         <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Main />} />
-            <Route path="/cart" element={<Cart />} />
-        </Routes>
-        </BrowserRouter>
-
-        
+            <Navbar/>
+            <Routes>
+                <Route path="/" element={<Main />} />
+                <Route path="/cart" element={<Cart />} />
+            </Routes>
+        </BrowserRouter>        
     </div>
   )
 }
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,9 +1,56 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
-const Navbar = () => {
+import styled from "styled-components";
+
+import logoElem from "../assets/poke(4).png";
+
+function Navbar() {
+
   return (
-    <div>Navbar</div>
-  )
+    <Header>
+
+    <Link/>
+      <Wrapper>
+        <Link to={"/"} aria-label="go home">
+          <img src={logoElem} alt="icon" className="imgWidth"/>
+        </Link>
+        <Nav>
+          <Link to={"/"} aria-label="go home">홈</Link>
+          <Link to={"/cart"} aria-label="go cart">장바구니</Link>
+        </Nav>
+      </Wrapper>
+    </Header>
+  );
 }
 
-export default Navbar
+const Header = styled.header`
+  width: 100%;
+  height: 72px;
+  border-bottom: 1px solid #ccc;
+  position: fixed;
+  top: 0;
+  z-index: 9999;
+  background-color: #fff;
+`;
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  max-width: 1024px;
+  padding: 0 16px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 0 auto;
+`;
+const Nav = styled.nav`
+  max-width: 1024px;
+  display: flex;
+  justify-content: right;
+  gap: 36px;
+  align-items: center;
+  > span {
+    font-weight: bold;
+    cursor: pointer;
+  }
+`;
+export default Navbar;
