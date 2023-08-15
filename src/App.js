2890045e8ca662ded0308c9b86737708a9a854ef
IG <iikim2511@gmail.com>
2023-08-14 22:57:20 -0400

    app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from './component/Navbar';
+import Main from './component/Main';
+import Cart from './component/Cart';
+
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+    
+        <h2>App</h2>
+
+        <Navbar/>
+        <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/cart" element={<Cart />} />
+        </Routes>
+        </BrowserRouter>
+
+        
+    </div>
   )
 }
 
